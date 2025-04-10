bcaaed882606cf4ea40d7c85e793d4fd0f84d6f7
Tsukasa-snhr <tsukasasandesu@gmail.com>
2025-04-10 22:12:43 +0900

    Add files via upload

---
+++ MyJava/ComprehensiveBeginner01_02.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class ComprehensiveBeginner01_02 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int[] nums = new int[10];
+        int sum = 0;
+        int count = 0;
+
+        int num;
+        while (sum <= 100 && count < 10) {
+            num = rand.nextInt(100) + 1;
+            nums[count] = num;
+            sum = sum + num;
+            count = count + 1;
+        }
+        System.out.println("合計=" + sum);
+        System.out.println("入力された数値=");
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("入力された数値=" + nums[i]);
+
+        }
+
+    }
+}
